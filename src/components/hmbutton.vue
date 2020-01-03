1783b7445f6a2736cb9b398c5c7f68f4792f3e54
蔡丽纯 <12345.qq.com>
2020-01-03 14:27:12 +0800

    04-封装按钮，主要使用了父传子和插槽

---
+++ src/components/hmbutton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="btn" @click="handlerclick">
+      <!-- 使用匿名插槽 -->
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        handlerclick(event){
+            // 如果按钮被点击了，要告诉父组件用户点击了按钮
+            this.$emit('click',event)
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+.btn {
+    width: 318*100/360;
+    height: 48px;
+    background-color: #cc3300;
+    color: #fff;
+    border-radius: 24px;
+    line-height: 50px;
+    text-align: center;
+    font-size: 20px;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,13 +15,28 @@
         没有账号？
         <a href="#/register" class>去注册</a>
       </p>
-      <div data-v-4bc01e24 class="button">登录按钮</div>
+      <!-- <div data-v-4bc01e24 class="button">登录按钮</div> -->
+        <!-- 使用自己封装的hmbutton按钮 -->
+        <!-- 监听子组件的click事件 -->
+        <hmbutton @click="login">登陆</hmbutton>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+// 引入自己封装的按钮
+import hmbutton from '@/components/hmbutton.vue'
+export default {
+    // 注册
+    components:{
+        hmbutton
+    },
+    methods:{
+        login(){
+            console.log('123');
+        }
+    }
+};
 </script>
 
 <style lang='less' scoped>
