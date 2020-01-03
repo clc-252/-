fa717832b2b1925c57196a9ec7f7c71c67d33647
蔡丽纯 <12345.qq.com>
2020-01-03 18:15:33 +0800

    实现文本框的封装，但还没有实现合理的提示信息功能

---
+++ src/components/hminput.vue
@@ -0,0 +1,58 @@
+<template>
+  <input
+    type="text"
+    class="hminput"
+    :class="{success:statu,error:!statu}"
+    @input="handleInput"
+    @blur="handleBlur"
+  />
+</template>
+
+<script>
+export default {
+  props: ["rules", "msg"],
+  data() {
+    return {
+      statu: true
+    };
+  },
+  methods: {
+    // 监听用户输入的数据，使用正则表达式验证数据是否正确，如果正确添加success的样式，如果错误添加error的样式
+    handleInput(event) {
+      let value = event.target.value;
+      if (this.rules && this.rules.test(value)) {
+        this.statu = true;
+      } else {
+        this.statu = false;
+      }
+      // 告诉父组件内容发生变化
+      this.$emit("input", value);
+    },
+    // 当文本框失焦的时候再验证一次，如果数据不合格，则提示用户
+    handleBlur() {
+      let value = event.target.value;
+      if (this.rules && !this.rules.test(value)) {
+        console.log(this.msg || "输入不正确");
+      }
+    }
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.hminput {
+  width: 318/360 * 100vw;
+  height: 60px;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #757575;
+  font-size: 22px;
+  line-height: 60px;
+}
+.success {
+  border-bottom-color: green;
+}
+.error {
+  border-bottom-color: red;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,34 +8,64 @@
         <span class="iconfont iconnew"></span>
       </div>
       <div class="inputs">
-        <input placeholder="请输入手机号" class="input" />
-        <input placeholder="密码" class="input" type="password" />
+        <!-- <input placeholder="请输入手机号" class="input" /> -->
+        <!-- 使用自己封装的hminput文本框 -->
+        <hminput
+          placeholder="请输入用户名或手机号码"
+          :rules="/^(\d{5,6})$|^(1\d{10})$/"
+          @input="handleinput"
+          msg="用户名或手机号不正确"
+          :value="users.username"
+        ></hminput>
+        <!-- <input placeholder="密码" class="input" type="password" /> -->
+        <hminput 
+          placeholder="请输入密码" 
+          :rules="/^\S{3,16}$/" 
+          msg="密码不正确" 
+          v-model="users.password"></hminput>
       </div>
       <p class="tips">
         没有账号？
         <a href="#/register" class>去注册</a>
       </p>
       <!-- <div data-v-4bc01e24 class="button">登录按钮</div> -->
-        <!-- 使用自己封装的hmbutton按钮 -->
-        <!-- 监听子组件的click事件 -->
-        <hmbutton @click="login">登陆</hmbutton>
+      <!-- 使用自己封装的hmbutton按钮 -->
+      <!-- 监听子组件的click事件 -->
+      <hmbutton @click="login">登陆</hmbutton>
     </div>
   </div>
 </template>
 
 <script>
 // 引入自己封装的按钮
-import hmbutton from '@/components/hmbutton.vue'
+import hmbutton from "@/components/hmbutton.vue";
+// 引入自己封装的文本框
+import hminput from "@/components/hminput.vue";
 export default {
-    // 注册
-    components:{
-        hmbutton
+  data() {
+    return {
+      users: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  // 注册
+  components: {
+    hmbutton,
+    hminput
+  },
+  methods: {
+    login() {
+      // console.log("123");
+      // 点击登陆按钮时，要获得用户输入的用户名和密码
+      console.log(this.users);
     },
-    methods:{
-        login(){
-            console.log('123');
-        }
+    handleinput(data) {
+      console.log(data);
+      this.users.username = data;
     }
+  }
 };
 </script>
 
