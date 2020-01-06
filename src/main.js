84b766e5d93bf2bd3a66914a3cb23d63784f4a0b
蔡丽纯 <12345.qq.com>
2020-01-06 20:41:01 +0800

    18-完成首页顶部栏和tab标签页的制作

--- src/main.js
+++ src/main.js
@@ -9,13 +9,15 @@ Vue.config.productionTip = false
 import router from '@/router/index.js'
 
 // 引入vant框架
-import { Toast,Icon,Uploader,Dialog,Field,Picker } from 'vant'
+import { Toast,Icon,Uploader,Dialog,Field,Picker,Tab,Tabs } from 'vant'
 Vue.use(Toast) // Toast 轻提示
    .use(Icon)  // Icon 图标
    .use(Uploader) // Uploader 文件上传
    .use(Dialog)  // Dialog 弹出框
    .use(Field)  // Field 输入框
    .use(Picker) // Picker 选择器
+   .use(Tab)  // Tab 标签页
+   .use(Tabs)
 
 new Vue({
   // 注入
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,71 @@
 <template>
-  <div class="index">浏览新闻主页</div>
+  <div class="index">
+    <!-- 头部 -->
+    <div class="header">
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="search">
+        <i class="iconfont iconsearch"></i>&nbsp;
+        <span>搜索新闻</span>
+      </div>
+      <div class="user">
+        <van-icon name="manager-o" />
+      </div>
+    </div>
+    <!-- 标签栏 -->
+    <div class="nav">
+      <!-- 通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签 -->
+      <van-tabs v-model="active" swipeable>
+        <van-tab title="标签 1">内容 1</van-tab>
+        <van-tab title="标签 2">内容 2</van-tab>
+        <van-tab title="标签 3">内容 3</van-tab>
+        <van-tab title="标签 4">内容 4</van-tab>
+      </van-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      active: 1
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.header {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #f00;
+  align-items: center;
+  color: #fff;
+  .logo {
+    padding: 0 10px;
+  }
+  .iconnew {
+    font-size: 60px;
+  }
+  .search {
+    flex: 1;
+    height: 36px;
+    border-radius: 18px;
+    background: rgba(255, 255, 255, 0.5);
+    line-height: 36px;
+    text-align: center;
+    font-size: 14px;
+    .icon_search {
+      font-size: 18px;
+    }
+  }
+  .user {
+    font-size: 26px;
+    padding: 0 10px 0 20px;
+  }
+}
 </style>
